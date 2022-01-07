e468f359cf55605f3953b06fc3b9540ec5ee5fe9
Henrich Krämer <henrich.kraemer@gmail.com>
2022-01-07 11:58:46 +0100

    M1 related aliases

--- zsh/sourced/aliases.zsh
+++ zsh/sourced/aliases.zsh
@@ -1,1 +1,16 @@
 alias ll='ls -lAh'
+alias ibrew='arch -x86_64 /usr/local/bin/brew'
+alias intel='arch -x86_64'
+
+# 
+alias archdocker="docker info -f '{{.OSType}}/{{.Architecture}}'"
+alias archdockervalues='echo "docker desktop architecture values:\nlinux/aarch64 (M1) linux/x86_64 (MBP intel)"'
+
+# candidates for alias
+
+# docker image inspect postgres:12 -f '{{.Os}}/{{.Architecture}}'
+# linux/arm64
+# → docker image inspect atlassian/bitbucket:7.6.5 -f '{{.Os}}/{{.Architecture}}'
+# linux/amd64                                                                                                                                                                                                                                                  
+# → docker info -f '{{.OSType}}/{{.Architecture}}'
+# linux/aarch64
